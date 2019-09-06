b33ef95045a57225c30e40c951c1f236dd25b1fc
Денис Котляров <denis2005991@gmail.com>
2019-09-06 21:58:31 +0300

    1. Macro for concatenation of lines (full_concat, concat) are transferred to separate library cluStrConcat, for giving of bigger correctness of Api.

--- src/lib.rs
+++ src/lib.rs
@@ -23,41 +23,57 @@ Safe constant combination of constant data.
 
 1. Easy
 
-```
+```rust
 #[macro_use]
 extern crate cluConstData;
 
 const_data! {
-	const S_PREFIX:			&'static str	= "L[";
-	const E_PREFIX:			&'static str 	= "]";
+	const A: &'static str	  = "[";
+	const B: &'static str	  = "]";
 	
-	const MY_STR:			&'static str	= S_PREFIX, "->", E_PREFIX;
-	const TWO_MY_STR:			&'static str	= MY_STR, MY_STR;
+	pub (crate) const RESULT: &'static str = A, "DATA", B;
 }
 
 fn main() {
-	println!("S_PREFIX: {:?}", S_PREFIX);
-	assert_eq!(S_PREFIX, "L[");
-	assert_eq!(S_PREFIX.len(), 2);
-	assert_eq!(S_PREFIX.as_bytes(), b"L[");
+	assert_eq!(A, "[");
+	assert_eq!(B, "]");
 	
-	
-	println!("E_PREFIX: {:?}", S_PREFIX);
-	assert_eq!(E_PREFIX, "]");
-	assert_eq!(E_PREFIX.len(), 1);
-	assert_eq!(E_PREFIX.as_bytes(), b"]");
-	
-	println!("MY_STR: {:?}", MY_STR);
-	assert_eq!(MY_STR, "L[->]");
-	assert_eq!(MY_STR.len(), 5);
-	assert_eq!(MY_STR.as_bytes(), b"L[->]");
-	
-	println!("TWO_MY_STR: {:?}", TWO_MY_STR);
-	assert_eq!(TWO_MY_STR, "L[->]L[->]");
-	assert_eq!(TWO_MY_STR.len(), 10);
-	assert_eq!(TWO_MY_STR.as_bytes(), b"L[->]L[->]");
+	assert_eq!(RESULT, "[DATA]");
 }
 ```
+
+2. EasyArray
+
+```rust
+#[macro_use]
+extern crate cluConstData;
+
+
+const_data! {
+	const U32_HEAD:	u32			= 255;
+	const U32_END:		u32			= 0;
+
+
+	const U32_ARRAY:	[u32; 3]		= &[U32_HEAD], &[2], &[U32_END];
+	const U32_SARRAY:	&'static [u32]	= &[U32_HEAD, 2, 3 ,4], &[2, 3], &[U32_END];	
+}
+
+fn main() {
+	println!("U32_HEAD: {:?}", U32_HEAD);
+	assert_eq!(U32_HEAD, 255);
+	
+	println!("U32_END: {:?}", U32_END);
+	assert_eq!(U32_END, 0);
+	
+	//arrays
+	println!("U32_ARRAY: {:?}", U32_ARRAY);
+	assert_eq!(U32_ARRAY, [255, 2, 0]);
+	
+	println!("U32_SARRAY: {:?}", U32_SARRAY);
+	assert_eq!(U32_SARRAY, [255, 2, 3, 4, 2, 3, 0]);
+}
+```
+
 
 2. ArrayUse
 
